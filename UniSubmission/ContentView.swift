6edcb29b119dd40001e7f00482172231c505eeff
ZenRega38 <regarizz@gmail.com>
2020-11-10 10:04:22 +0800

    Initial Commit

---
+++ UniSubmission/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniSubmission
+//
+//  Created by Rega Rizkan Azizan on 10/11/20.
+//  Copyright © 2020 rega. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
